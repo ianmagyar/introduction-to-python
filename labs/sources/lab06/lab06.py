4fc7bb9f8465071a474cf887f5a5735d11369a6e
ianmagyar <jmagyar1993@gmail.com>
2026-03-23 07:31:47 +0100

    published lab 6

---
+++ labs/sources/lab06/lab06.py
@@ -0,0 +1,25 @@
+import random
+
+
+def load_words(dataset_path):
+    return list()
+
+
+def get_puzzle(word_list):
+    return ""
+
+
+def is_game_finished(guess, puzzle):
+    return False
+
+
+def evaluate_guess(guess, puzzle):
+    return list()
+
+
+def start_game(dataset_path):
+    pass
+
+
+if __name__ == '__main__':
+    start_game("word_list.txt")
