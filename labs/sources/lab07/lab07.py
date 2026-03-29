da653e91425c6b1f3a597c16d19408ca366940f9
ianmagyar <jmagyar1993@gmail.com>
2026-03-29 20:49:09 +0200

    published lab 7

---
+++ labs/sources/lab07/lab07.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import random
+import math
+from numpy import arange
+
+
+def calculate_pi(in_circle, total):
+    return 0
+
+
+def is_in_circle(x, y):
+    return False
+
+
+def throw_darts(num_darts, show_plot):
+    in_circle = 0
+    estimates = list()
+
+    return 0
+
+
+def plot_estimates(estimates):
+    pass
+
+
+if __name__ == '__main__':
+    darts = 1000000
+    pi_guess = throw_darts(darts, True)
+    print("Estimated value of pi with {} darts is {}".format(darts, pi_guess))
